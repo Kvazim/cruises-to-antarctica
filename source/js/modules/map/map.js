6631d3b0810895a732c1ec8233ab8175af54ebb0
Kvazim <mesvladimir@gmail.com>
2022-11-06 11:47:41 +0300

    Добавление балуна на карту

--- source/js/modules/map/map.js
+++ source/js/modules/map/map.js
@@ -1,41 +1,36 @@
-// const
+const map = document.querySelector('.contacts__map');
 
 const CENTER_MAP = {
   lat: 59.939352,
   lng: 30.323414,
 };
 
-const ZOOM = 17;
+const PIN_CENTER = {
+  lat: 59.938635,
+  lng: 30.323118,
+}
+
+const ZOOM = 16;
 
 ymaps.ready(init);
 
 function init() {
-  const myMap = new ymaps.Map('map', {
-    center: [CENTER_MAP.lat, CENTER_MAP.lng], // Москва
+  const myMap = new ymaps.Map(map, {
+    center: [CENTER_MAP.lat, CENTER_MAP.lng],
     zoom: ZOOM,
   });
+
+  myPlacemark = new ymaps.Placemark([PIN_CENTER.lat, PIN_CENTER.lng], {
+    balloonContentHeader: '<h2>Круизы в Антарктику</h2>',
+    balloonContentBody: 'г. Санкт-Петербург,<br> ул. Большая Конюшенная, 19/8',
+    balloonContentFooter: 'Наш телефон:<br> 8-800-200-00-00',
+    hintContent:'Круизы в Антарктику'
+      }, {
+          iconImageHref: '../img/svg/map-pin.svg',
+          iconImageSize: [18, 22],
+          iconImageOffset: [0, -22],
+          iconLayout: 'default#image',
+      });
+
+      myMap.geoObjects.add(myPlacemark);
 }
-
-// var myMap;
-
-// // Дождёмся загрузки API и готовности DOM.
-// ymaps.ready(init);
-
-// function init () {
-//     // Создание экземпляра карты и его привязка к контейнеру с
-//     // заданным id ("map").
-//     myMap = new ymaps.Map('map', {
-//         // При инициализации карты обязательно нужно указать
-//         // её центр и коэффициент масштабирования.
-//         center: [55.76, 37.64], // Москва
-//         zoom: 10
-//     }, {
-//         searchControlProvider: 'yandex#search'
-//     });
-
-//     document.getElementById('destroyButton').onclick = function () {
-//         // Для уничтожения используется метод destroy.
-//         myMap.destroy();
-//     };
-
-// }
