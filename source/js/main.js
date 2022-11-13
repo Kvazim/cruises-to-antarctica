b681693d079e9b500fb191315c456b52342fe8e0
Vladimir <mesvladimir@yandex.ru>
2022-11-13 17:17:05 +0300

    litle fix

--- source/js/main.js
+++ source/js/main.js
@@ -16,12 +16,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   removeNoJs();
-  createMap();
   // ---------------------------------
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    createMap();
     initModals();
   });
 });
