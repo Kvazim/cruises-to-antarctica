626eb70e11d3750afd11f25fc2d9bab3920d1995
Vladimir <mesvladimir@yandex.ru>
2022-11-15 18:01:53 +0300

    fix bugs link klick finish?

--- source/js/modules/smooth-scroll/smooth-scroll.js
+++ source/js/modules/smooth-scroll/smooth-scroll.js
@@ -1,8 +1,14 @@
+import {navigation, closeMenu} from '../toggle-button/togle-button';
+
 const buttonsScroll = document.querySelectorAll('[data-scroll]');
 
 for (let buttonScroll of buttonsScroll) {
   buttonScroll.addEventListener('click', function (e) {
     e.preventDefault();
+
+    if (navigation.contains(e.target)) {
+      closeMenu();
+    }
     const link = buttonScroll.getAttribute('href');
 
     document.querySelector(link).scrollIntoView({
--- source/js/modules/toggle-button/togle-button.js
+++ source/js/modules/toggle-button/togle-button.js
@@ -3,7 +3,6 @@ import {header} from '../nojs/nojs';
 
 const navigationToggle = document.querySelector('[data-navigation-toggle]');
 const navigation = document.querySelector('[data-navigation]');
-const buttonsMenu = document.querySelectorAll('[data-menu]');
 const promo = document.querySelector('.promo');
 
 const scrollLock = new ScrollLock();
@@ -14,7 +13,6 @@ const openMenu = () => {
   promo.style.marginTop = '62px';
   scrollLock.disableScrolling();
   window.addEventListener('mousedown', onWindowClick);
-  // addLinkClick();
 };
 
 const closeMenu = () => {
@@ -23,7 +21,6 @@ const closeMenu = () => {
   promo.style.marginTop = null;
   scrollLock.enableScrolling();
   window.removeEventListener('mousedown', onWindowClick);
-  // removeLinkClick();
 };
 
 const onToggleClick = () => {
@@ -35,14 +32,6 @@ const onToggleClick = () => {
     return;
   }
 };
-
-// const addLinkClick = () => {
-//   buttonsMenu.forEach((buttonMenu) => buttonMenu.addEventListener('click', closeMenu));
-// };
-
-// const removeLinkClick = () => {
-//   buttonsMenu.forEach((buttonMenu) => buttonMenu.removeEventListener('click', closeMenu));
-// };
 
 const onWindowClick = ((e) => {
   if (e.target !== navigation && !navigation.contains(e.target)) {
@@ -60,12 +49,10 @@ const breakpointToggle = window.matchMedia('(min-width:768px)');
 const breakpointChecker = () => {
   if (breakpointToggle.matches) {
     navigationToggle.removeEventListener('click', onToggleClick);
-    buttonsMenu.forEach((buttonMenu) => buttonMenu.removeEventListener('click', closeMenu));
   } else {
     navigationToggle.addEventListener('click', onToggleClick);
-    buttonsMenu.forEach((buttonMenu) => buttonMenu.addEventListener('click', closeMenu));
   }
 };
 breakpointToggle.addListener(breakpointChecker);
 
-export {initToggle};
+export {initToggle, navigation, closeMenu};
